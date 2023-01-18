636c06cfd00889276beb92217e810aab461712d8
Peter Hardy <PHardy_a@fhlbc.com>
2023-01-18 13:08:46 -0600

    Learning about using variables in depth

---
+++ 02 - Variables/Variables/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const float Pi = 3.14f;
+            
+            var number = 2;
+            var count = 10;
+            var totalPrice = 20.95f;
+            var character = 'A';
+            var firstName = "Peter";
+            var isWorking = false;
+
+            Console.WriteLine(Pi);
+            
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            Console.WriteLine("{0} {1}", float.MinValue, float.MaxValue);
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
