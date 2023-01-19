a113220bac57717ef238432af2b8cc0deb699acb
Peter Hardy <PHardy_a@fhlbc.com>
2023-01-18 21:20:59 -0600

    Type conversion demo with a try and except block

---
+++ 03 - TypeConversion/TypeConversion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeConversion
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var number = "1234";
+                byte b = Convert.ToByte(number);
+
+                Console.WriteLine(b);
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("The number could no be converted to a byte.");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
