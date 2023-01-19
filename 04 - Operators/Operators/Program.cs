4ac826ba7cb9d1c390dc855b9d2e4fd351c51a0a
Peter Hardy <PHardy_a@fhlbc.com>
2023-01-18 21:36:06 -0600

    Operator basics

---
+++ 04 - Operators/Operators/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operators
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            var b = 2;  
+            var c = 3;
+
+            Console.WriteLine(!(c > b || c == a));
+            Console.ReadLine();
+        }
+    }
+}
