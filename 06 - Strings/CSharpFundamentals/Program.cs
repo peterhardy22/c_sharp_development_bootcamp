9fe9b256520b873fcffed784046013bf0cca0453
Peter Hardy <PHardy_a@fhlbc.com>
2023-01-24 22:14:57 -0600

    Verbatim string @ for not using escape characters rules

---
+++ 06 - Strings/CSharpFundamentals/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFundamentals
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var firstName = "Peter";
+            var lastName = "Harty";
+
+            var fullName = string.Format("My name is {0} {1}", firstName, lastName);
+            Console.WriteLine(fullName);
+
+            var names = new string[3] { "Cassie", "Mickie", "Josie" };
+            var formattedNames = string.Join(", ", names);
+            Console.WriteLine(formattedNames);
+
+            var text = @"Hi Clyde
+Look into the following paths
+c:\Accounts\Betty
+c:\Finances\Timmy";
+            Console.WriteLine(text);
+            Console.ReadLine(); 
+        }
+    }
+}
