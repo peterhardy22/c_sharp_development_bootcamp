5958f92f37cbe9d52ba3c818b2c6211cdcc8e24c
Peter Hardy <PHardy_a@fhlbc.com>
2023-01-25 22:25:32 -0600

    foreach loops all the way

---
+++ 09 - Iterations/Iterations/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Iterations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var name = "Peatear Hearte";
+
+            foreach (var character in name)
+            {
+                Console.WriteLine(character);
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
